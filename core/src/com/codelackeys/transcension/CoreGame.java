14a90650115e3b01dba547de08ba645e6b5140e7
BuildTools <unconfigured@null.spigotmc.org>
2017-07-27 10:37:07 -0400

    initial commit

---
+++ core/src/com/codelackeys/transcension/CoreGame.java
@@ -0,0 +1,22 @@
+package com.codelackeys.transcension;
+
+import com.badlogic.gdx.Game;
+import com.codelackeys.transcension.screens.GameScreen;
+
+public class CoreGame extends Game {
+	
+	@Override
+	public void create () {
+		setScreen(new GameScreen());
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
