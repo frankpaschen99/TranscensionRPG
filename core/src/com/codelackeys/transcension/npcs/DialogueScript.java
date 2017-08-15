21b7bdc9eea93d0a68b6a770a297353bc025372d
Frank <frankpaschen99@gmail.com>
2017-08-15 03:16:01 -0400

    comments

--- core/src/com/codelackeys/transcension/npcs/DialogueScript.java
+++ core/src/com/codelackeys/transcension/npcs/DialogueScript.java
@@ -16,13 +16,18 @@ public class DialogueScript {
 		// im writing this and never looking at it again - 8/15/17
 		JsonReader json = new JsonReader();
 		JsonValue base = json.parse(Gdx.files.internal(dialogueFile));
+		
+		// for every object in "speech" array...
 		for (JsonValue entries : base.get("speech")) {
-			DialogueLine l = new DialogueLine();
-			l.dialogueText = entries.getString("text");
+			// create a  new DialogueLine and populate it with the
+			// NPC text, and possible player responses
+			DialogueLine dL = new DialogueLine();
+			dL.dialogueText = entries.getString("text");
 			for (JsonValue responses : entries.get("responses")) {
-				l.responses.add(new DialogueResponse(Integer.parseInt(responses.getString("jumpto"))
+				dL.responses.add(new DialogueResponse(Integer.parseInt(responses.getString("jumpto"))
 						, responses.getString("text")));
 			}
+			script.add(dL);	// add it to the script
 		}
 	}
 	public void advanceDialogue(int jumpTo) {
