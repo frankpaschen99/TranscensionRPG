000a451739c3919f338420fa2fe1b9a6fea59611
Frank Paschen <frankpaschen99@gmail.com>
2017-10-16 20:47:19 -0400

    some good stuff

--- core/src/com/codelackeys/transcension/dialogue/DialogueLine.java
+++ core/src/com/codelackeys/transcension/dialogue/DialogueLine.java
@@ -3,9 +3,7 @@ package com.codelackeys.transcension.dialogue;
 import java.util.ArrayList;
 import java.util.List;
 
-/* A dialogue line represents one part of a conversation with an NPC.
-   
-*/
+/* A dialogue line represents one part of a conversation with an NPC. */
 
 public class DialogueLine {
 	public String dialogueText;
---
+++ core/src/com/codelackeys/transcension/entities/weapons/Taser.java
@@ -0,0 +1,7 @@
+package com.codelackeys.transcension.entities.weapons;
+
+import com.codelackeys.transcension.entities.Weapon;
+
+public class Taser extends Weapon {
+
+}
